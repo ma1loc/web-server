581d7fcb4eb835a57ebf61ddcbf87ca1e43f4cea
Abdellah Benzahour <abde.benz290@gmail.com>
2026-03-05 17:10:37 +0000

    add cgi and add cgi with attrebute and methd to check for cgi is need or not

--- cgi/cgi.cpp
+++ cgi/cgi.cpp
@@ -1,2 +1,70 @@
 #include "cgi.hpp"
 
+Cgi::Cgi()
+{
+}
+
+Cgi::Cgi(const Cgi &other)
+{
+    *this = other;
+}
+
+Cgi &Cgi::operator=(const Cgi &other)
+{
+    if (this != &other)
+    {
+        interpreter = other.interpreter;
+        extension   = other.extension;
+    }
+    return *this;
+}
+
+Cgi::~Cgi()
+{
+}
+
+void Cgi::setInterpreter(const std::string &interpreter)
+{
+    this->interpreter = interpreter;
+}
+
+void Cgi::setExtension(const std::string &extension)
+{
+    this->extension = extension;
+}
+
+std::string Cgi::getInterpreter() const
+{
+    return interpreter;
+}
+
+std::string Cgi::getExtension() const
+{
+    return extension;
+}
+
+bool Cgi::checkForCgi(Client &client)
+{
+    if (client.location_conf->cgi_handler.empty())
+        return false;
+
+    size_t dot = client.req.getPath().rfind('.');
+
+    if (dot == std::string::npos)
+        return false;
+
+    std::string exten = client.req.getPath().substr(dot);
+    std::map<std::string, std::string>::const_iterator it =
+        client.location_conf->cgi_handler.begin();
+
+    for (; it != client.location_conf->cgi_handler.end(); it++)
+    {
+        if (exten == it->first)
+        {
+            interpreter = it->second;
+            extension   = it->first;
+            return true;
+        }
+    }
+    return false;
+}
--- cgi/cgi.hpp
+++ cgi/cgi.hpp
@@ -1,10 +1,27 @@
 #ifndef CGI_HPP
 #define CGI_HPP
 
+#include "../client.hpp"
+
 class Cgi
 {
-	private:
-	public:
+  private:
+    std::string interpreter;
+    std::string extension;
+
+  public:
+    Cgi();
+    Cgi(const Cgi &other);
+    Cgi &operator=(const Cgi &other);
+    ~Cgi();
+
+    void setInterpreter(const std::string &interpreter);
+    void setExtension(const std::string &extension);
+
+    std::string getInterpreter() const;
+    std::string getExtension() const;
+
+	bool checkForCgi(Client &client);
 };
 
 #endif
