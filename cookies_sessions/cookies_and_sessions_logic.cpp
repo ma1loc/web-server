baa0b333301bf06243582d1b814f79dd0806d5a8
ybenchel <yass515bec@outlook.fr>
2026-04-15 13:35:14 +0100

    added time check for cookies

---
+++ cookies_sessions/cookies_and_sessions_logic.cpp
@@ -0,0 +1,35 @@
+#include "cookies_and_sessions_logic.hpp"
+
+Session& cookies_and_sessions_logic(SessionManager& sessionM, Client& client)
+{
+    std::string key;
+    std::string CookieHeader;
+    std::string SessionID;
+    std::map<std::string, std::string> data;
+
+    
+    if (client.req.getHeaders().find("COOKIE") != client.req.getHeaders().end())
+    {
+        CookieHeader = client.req.getHeaders()["COOKIE"];
+        data = parseCookies(CookieHeader);
+        if (!data["sessionId"].empty())
+            SessionID = data["sessionId"];
+        else
+            return sessionM.createSession();
+        if (sessionM.sessionExists(SessionID))
+        {
+            sessionM.sessionTimeCheck(SessionID);
+            if(sessionM.sessionExists(SessionID))
+            {
+                Session& Ses = sessionM.getSession(SessionID);
+                Ses.is_new = false;
+                Ses.last_access = std::time(0);
+                return Ses;
+            }else
+                return sessionM.createSession();
+        }
+        else
+            return sessionM.createSession();
+    }else
+        return sessionM.createSession();
+}
